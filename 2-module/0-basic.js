5040f3bd5e5ec41c5629c82b17ac5107e4e4915e
etim-ipee <imaikopelijah001@gmail.com>
2025-01-23 12:14:36 +0100

    javascript module

---
+++ 2-module/0-basic.js
@@ -0,0 +1,7 @@
+console.log("Hello World!")
+let fullName= "Victor Vincent"
+console.log("Hello " + fullName)
+fullName= "Monday Uche"
+console.log(`Hello ${fullName}`)
+const PI= 3.143
+console.log(PI)
