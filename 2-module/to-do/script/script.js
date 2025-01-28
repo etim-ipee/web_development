f08f00f0cec2d019ccda2b091f1ee67415ef3e84
etim-ipee <imaikopelijah001@gmail.com>
2025-01-28 15:20:14 +0100

    update

--- 2-module/to-do/script/script.js
+++ 2-module/to-do/script/script.js
@@ -1,7 +1,7 @@
 const form= document.getElementById('todo-form');
 const input= document.getElementById('todo-input');
 const list= document.getElementById('todo-list');
-const button=document.getElementById('btn');
+
 
 /**
  * addTodo - A function that adds todo to the list
@@ -11,13 +11,25 @@ const addTodo = (event) => {
     const text = input.value.trim()
     if (text !== ""){
         const item = document.createElement("li")
-        item.innerText = text
+        item.innerHTML = `
+        <div>
+            <input type='checkbox' />
+            <label>${text}</label>
+            <button id='btn'>Delete</button>
+        </div>
+        `
         list.appendChild(item)
         input.value = ""
-        
+
+        // Get delete button
+        const button=document.getElementById('btn');
+        button.addEventListener('click',deleteTodo)
     }
 }
 const deleteTodo = () => {
-    alert("Delete functionality not implemented yet")
+    //alert("Delete functionality not implemented yet")
+    const parent = document.getElementById('btn');
+    console.log(parent.parentElement.parentElement)
+    list.removeChild(parent.parentElement.parentElement)
 }
 form.addEventListener("submit", addTodo)
