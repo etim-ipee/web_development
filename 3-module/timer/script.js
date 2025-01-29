c38a9d337b0681f6f78fd797aff9688498920951
etim-ipee <imaikopelijah001@gmail.com>
2025-01-29 18:12:07 +0100

    update

---
+++ 3-module/timer/script.js
@@ -0,0 +1,22 @@
+const timerInput = document.getElementById('timerInput')
+const starterTimer = document.getElementById('startTimer')
+const countdownDisplay = document.getElementById('countdownDisplay')
+
+const counter = () =>{
+    let timeRemaining = parseInt(timerInput.value)
+    if (timeRemaining <= 0 || isNaN(timeRemaining)){
+        return
+    }
+    timerInput.value = 0
+    countdownDisplay.textContent= timeRemaining
+    const intervalId = setInterval(() => {
+        timeRemaining -=1
+        countdownDisplay.textContent = timeRemaining
+        if (timeRemaining <= 0){
+            clearInterval(intervalId)
+            countdownDisplay.textContent = 'TIME UP!'
+        }
+    }, 1000);  
+}
+
+starterTimer.addEventListener('click', counter)
