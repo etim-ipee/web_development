dc1e1345e2b514b2476f5173f864edc87431f2fa
etim-ipee <imaikopelijah001@gmail.com>
2025-02-06 17:03:15 +0100

    server

--- 4-module/server.js
+++ 4-module/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = 5500;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
