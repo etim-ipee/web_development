f9ebee2434df1c3a7a206d0c9ee55c4e3e2c9941
etim-ipee <imaikopelijah001@gmail.com>
2025-02-06 15:49:56 +0100

    server side

---
+++ 4-module/reg-form/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/register', (req, res) => {
+  const formData = req.body;
+
+  // Save form data to a file
+  fs.writeFile('formData.json', JSON.stringify(formData, null, 2), (err) => {
+    if (err) {
+      return res.status(500).send('Server error');
+    }
+    res.send('Form data saved');
+  });
+});
+
+app.get('/register', (req, res) => {
+  // Retrieve form data from the file
+  fs.readFile('formData.json', (err, data) => {
+    if (err) {
+      return res.status(500).send('Server error');
+    }
+    res.send(JSON.parse(data));
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
