0864a20f4ef58b175f8d3b9f08dd8edcac97c03c
etim-ipee <imaikopelijah001@gmail.com>
2025-02-11 17:44:08 +0100

    updates

--- 5-module/node demo_readfile.js
+++ 5-module/node demo_readfile.js
@@ -1,7 +1,7 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fileSystem = require('fileSystem');
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
+  fileSystem.readFile('demofile1.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
