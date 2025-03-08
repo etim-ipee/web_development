a54e209cb41811041e9563ce012015da3a1bfa2c
etim-ipee <imaikopelijah001@gmail.com>
2025-03-08 15:14:31 +0100

    update

--- nodejsDB/demo_db_connection.js
+++ nodejsDB/demo_db_connection.js
@@ -1,12 +1,21 @@
 const mysql = require('mysql');
 
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "099444Bc#"
+const connection = mysql.createConnection({
+  host: 'localhost', // Your MySQL host
+  user: 'root', // Your MySQL username
+  password: '099444Bc#', // Your MySQL password
+  database: 'connector' // The database you want to connect to
 });
 
-con.connect(function(err) {
+connection.connect((err) => {
   if (err) throw err;
-  console.log("Connected!");
+  console.log('Connected to MySQL database!');
 });
+
+// Example query
+connection.query('SELECT * FROM connector', (err, results, fields) => {
+  if (err) throw err;
+  console.log(results);
+});
+
+connection.end();
